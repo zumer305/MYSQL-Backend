dbd526dad6c942704673d0f5eef089a1035d2034
zumer305 <zumerniaz305@gmail.com>
2025-09-29 00:06:54 +0500

    update

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 // npm init(package.json)
 // npm i @faker-js/faker
-const { faker } = require('@faker-js/faker');
-let  getRandomUser=()=> {
-  return [
-     faker.string.uuid(),
-     faker.internet.username(),
-     faker.internet.email(),
-     faker.internet.password(),
-  ];
+// const { faker } = require('@faker-js/faker');
+// let  getRandomUser=()=> {
+//   return [
+//      faker.string.uuid(),
+//      faker.internet.username(),
+//      faker.internet.email(),
+//      faker.internet.password(),
+//   ];
   
-}
+// }
 // console.log(getRandomUser());
 
 
@@ -27,7 +27,7 @@ let  getRandomUser=()=> {
 
 
 
-// npm i mysql2
+// // npm i mysql2
 const mysql = require('mysql2');
 // Create the connection to database
 const connection = mysql.createConnection({ //starting connection
@@ -36,26 +36,26 @@ const connection = mysql.createConnection({ //starting connection
   database: 'delta',
   password:'zumer2004',
 });
-let q='insert into user(id,username,email,password) values ?';
-let data=[];
-for(let i=1;i<=100;i++){
-    data.push(getRandomUser());
-}
-// let user=[['3','zumerr','zumerr@gmail.com','12345'],['2','ali','ali@gmail.com','123456']];
-try{
-    connection.query(q,[data],(err,result)=>{  //pacakgeinstall baki yah the (workbench,cli,sqlfiles)
-if(err) throw err;
-console.log(result);
-// console.log(result.length);
-// console.log(result[0]);
-// console.log(result[1]);
-    });
+// let q='insert into user(id,username,email,password) values ?';
+// let data=[];
+// for(let i=1;i<=100;i++){
+//     data.push(getRandomUser());
+// }
+// // let user=[['3','zumerr','zumerr@gmail.com','12345'],['2','ali','ali@gmail.com','123456']];
+// try{
+//     connection.query(q,[data],(err,result)=>{  //pacakgeinstall baki yah the (workbench,cli,sqlfiles)
+// if(err) throw err;
+// console.log(result);
+// // console.log(result.length);
+// // console.log(result[0]);
+// // console.log(result[1]);
+//     });
 
-}
-catch(err){
-    console.log(err);
-}
-connection.end(); //ending
+// }
+// catch(err){
+//     console.log(err);
+// }
+// connection.end(); //ending
 
 
 // "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
@@ -79,7 +79,34 @@ connection.end(); //ending
 
 // getRandomUser,patch(update username)
 // npm i express 
-// npm i uuid 
+// npm i uuid (for new data add)
+const express=require('express');
+const app=express();
+app.listen(8080,()=>{
+    console.log('app is listening');
+});
+app.get('/', (req, res) => {
+    let q = 'SELECT COUNT(*) as total FROM user';
+    
+    connection.query(q, (err, result) => {
+        if (err) {
+            console.log("Database Error: ", err);
+            return res.send('some error in database'); // stop further execution
+        }
+
+        let count =(result[0].total);
+        res.render('home.ejs',{count});
+    });
+});
+
+
+
+
+
+// npm i ejs 
+const path=require('path');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
 
 
 
