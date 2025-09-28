ac6f1dcfdc8785f010a3ce4f75019d0f7c4dc7c0
zumer305 <zumerniaz305@gmail.com>
2025-09-28 20:51:16 +0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// MYSQL+NODE .....................................
+
 // npm init(package.json)
 // npm i @faker-js/faker
-
 const { faker } = require('@faker-js/faker');
-
 let  getRandomUser=()=> {
   return {
     Id: faker.string.uuid(),
@@ -12,4 +12,36 @@ let  getRandomUser=()=> {
  
   };
 }
-console.log(getRandomUser());
+// console.log(getRandomUser());
+
+
+
+
+
+// npm i mysql2
+const mysql = require('mysql2');
+// Create the connection to database
+const connection = mysql.createConnection({ //starting connection
+  host: 'localhost',
+  user: 'root',
+  database: 'delta',
+  password:'zumer2004',
+});
+try{
+    connection.query('Show Tables',(err,result)=>{
+if(err) throw err;
+console.log(result);
+    });
+
+}
+catch(err){
+    console.log(err);
+}
+connection.end(); //ending
+// "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
+//  show databases;
+// use delta;
+// show tables;
+// node index.js;
+
+
