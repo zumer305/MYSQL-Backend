2302911dfcf4c5260756db3ab0b41a8681e11782
zumer305 <zumerniaz305@gmail.com>
2025-09-28 22:33:40 +0500

    update

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ let  getRandomUser=()=> {
 
 
 
+
+
+
+
+
+
+
+
+
 // npm i mysql2
 const mysql = require('mysql2');
 // Create the connection to database
@@ -27,10 +36,15 @@ const connection = mysql.createConnection({ //starting connection
   database: 'delta',
   password:'zumer2004',
 });
+let q='insert into user(id,username,email,password) values ?';
+let user=[['3','zumerr','zumerr@gmail.com','12345'],['2','ali','ali@gmail.com','123456']];
 try{
-    connection.query('Show Tables',(err,result)=>{
+    connection.query(q,[user],(err,result)=>{  //pacakgeinstall baki yah the (workbench,cli,sqlfiles)
 if(err) throw err;
 console.log(result);
+// console.log(result.length);
+// console.log(result[0]);
+// console.log(result[1]);
     });
 
 }
@@ -38,12 +52,23 @@ catch(err){
     console.log(err);
 }
 connection.end(); //ending
+
+
 // "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
+// password
+
+
+
 //  show databases;
 // use delta;
 // show tables;
 // node index.js;
 
 
+
+
+
+
+// source schema.sql; 
 
 
