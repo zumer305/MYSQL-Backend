ffa0994933d9a36f8f5bbd9f53c2d9f9ec010618
zumer305 <zumerniaz305@gmail.com>
2025-09-28 21:41:08 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // npm init(package.json)
 // npm i @faker-js/faker
 
+const { faker } = require('@faker-js/faker');
+
+let  getRandomUser=()=> {
+  return {
+    Id: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+ 
+  };
+}
+console.log(getRandomUser());
